1b1ff17f94df8629b511d461940accda351561fb
Huaizhi Ge <gehuaizhi@outlook.com>
2024-07-21 21:52:28 -0400

    let add be append

--- qlora.py
+++ qlora.py
@@ -256,7 +256,8 @@ def find_all_linear_names(args, model):
     for name, module in model.named_modules():
         if isinstance(module, cls):
             names = name.split('.')
-            lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+            # lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+            lora_module_names.append(names[0] if len(names) == 1 else names[-1])
 
 
     if 'lm_head' in lora_module_names: # needed for 16-bit
