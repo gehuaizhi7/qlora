141eb2059d5b1892e7e0bdad196624d35fee8f1e
Huaizhi Ge <60053346+gehuaizhi7@users.noreply.github.com>
2024-07-23 15:20:45 -0400

    Update qlora.py
    update layers_num

--- qlora.py
+++ qlora.py
@@ -251,18 +251,19 @@ class GenerationArguments:
 
 def find_all_linear_names(args, model):
     cls = bnb.nn.Linear4bit if args.bits == 4 else (bnb.nn.Linear8bitLt if args.bits == 8 else torch.nn.Linear)
-    # lora_module_names = set()
-    lora_module_names = []
+    lora_module_names = set()
+    # lora_module_names = []
     for name, module in model.named_modules():
         if isinstance(module, cls):
             names = name.split('.')
-            # lora_module_names.add(names[0] if len(names) == 1 else names[-1])
-            lora_module_names.append(names[0] if len(names) == 1 else names[-1])
+            lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+            # lora_module_names.append(names[0] if len(names) == 1 else names[-1])
 
 
     if 'lm_head' in lora_module_names: # needed for 16-bit
         lora_module_names.remove('lm_head')
     if args.layers_num != 0:
+        lora_module_names = list(lora_module_names)
         lora_module_names = lora_module_names[-args.layers_num:]
     return list(lora_module_names)
 
